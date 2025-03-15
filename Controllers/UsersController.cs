07ad719b82995f29414207a54afe6d9be0a33ee6
Erdaulet <erdauletakilzhan83@gmail.com>
2025-03-15 12:45:02 +0500

    Добавьте файлы проекта.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static List<User> users = new List<User>
+        {
+            new User { Id = 1, Name = "Иван", Age = 30 },
+            new User { Id = 2, Name = "Анна", Age = 25 }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUser(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+                return NotFound();
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public ActionResult<User> CreateUser([FromBody] User user)
+        {
+            user.Id = users.Max(u => u.Id) + 1;
+            users.Add(user);
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+    }
+}
